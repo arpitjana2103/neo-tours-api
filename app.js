ad2d288dc87e4c3ffe57c98642a18afeed68bb14
Arpit Jana <arpitjana2103@gmail.com>
2025-10-22 20:50:05 +0530

    feature (req.query) : query parsing with qs module

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const qs = require("qs");
 const tourRouter = require("./routes/tour.route");
 const { globalErrorHandeller } = require("./controllers/error.controller");
 
 const app = express();
+
+// req.query parser Middleware
+// Set a custom query parser using qs
+app.set("query parser", (str) => qs.parse(str));
 
 // req.body parser Middleware :
 // ‍[ note : Parses incoming JSON requests into JavaScript objects ]
