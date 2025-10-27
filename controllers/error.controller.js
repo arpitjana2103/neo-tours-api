509ec0e806f2a01d0576cf5c19b6958d55b26247
Arpit Jana <arpitjana2103@gmail.com>
2025-10-27 13:18:10 +0530

    feature (error.controller) : capture stack trace - Error.captureStackTrace

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -4,6 +4,18 @@ exports.AppError = class extends Error {
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
         this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor);
+
+        /*
+        Error.captureStackTrace(this, this.constructor) : Explanation
+        -------------------------------------------------------------
+        this → refers to the current instance of your AppError.
+        this.constructor → refers to the AppError class itself.
+
+        This tells JavaScript:
+        "Start the stack trace from this point, and exclude everything above this constructor."
+        */
     }
 };
 
