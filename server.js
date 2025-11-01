e87dd4f2ef659fa536c5db195122e910d24c5400
Arpit Jana <arpitjana2103@gmail.com>
2025-11-01 09:01:33 +0530

    feature (error) : Handled Unhandled Promise Rejections

--- server.js
+++ server.js
@@ -8,10 +8,26 @@ const DBLOC = process.env.DATABASE_LOCAL;
 const PORT = process.env.PORT || 6700;
 
 const server = app.listen(PORT, function () {
-  console.log("⌛ connecting to database...");
+    console.log("⌛ connecting to database...");
 
-  mongoose.connect(DBLOC).then(function () {
-    console.log("✅ database connecting successfull");
-    console.log(`🔗 api url : http://127.0.0.1:${PORT}`);
-  });
+    mongoose
+        .connect(DBLOC)
+        .then(function () {
+            console.log("✅ database connecting successfull");
+            console.log(`🔗 api url : http://127.0.0.1:${PORT}`);
+        })
+        .catch(function (err) {
+            console.log("(ノಠ益ಠノ) Database Connection Failed.");
+            console.log(err);
+        });
+});
+
+// Handlling Unhandled Promise Rejections
+process.on("unhandledRejection", function (err) {
+    console.log("UNHUNDELED REJECTION :: SHUTTING DOWN THE SERVER");
+    console.log(err);
+    // Shutting down gracefully.
+    server.close(function () {
+        process.exit(1);
+    });
 });
