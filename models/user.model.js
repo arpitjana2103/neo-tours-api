ff8fdbbe8946e22cd9e0c600c4bb981f9be118ae
Arpit Jana <arpitjana2103@gmail.com>
2025-11-08 17:40:49 +0530

    add (user) : user model

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "##-please-tell-us-your-name!-##"],
+    },
+    email: {
+        type: String,
+        required: [true, "##-please-provide-your-email-##"],
+        unique: true,
+        lowercase: true,
+        validate: {
+            validate: validator.isEmail,
+            message: ["##-please-provide-a-valid-email-##"],
+        },
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, "##-please-provide-a-password-##"],
+        minlength: 8,
+    },
+    passwordConfirmed: {
+        type: String,
+        required: [true, "##-please-confirm-your-passwrod-##"],
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
