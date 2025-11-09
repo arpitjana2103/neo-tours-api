d34079c58bffb56b2a75efed33679cb82dc9ded6
Arpit Jana <arpitjana2103@gmail.com>
2025-11-09 11:25:47 +0530

    feature (auth) : send jwt while sign-in user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,33 @@
 const User = require("./../models/user.model");
 const { catchAsyncErrors } = require("./error.controller");
 
-exports.signup = catchAsyncErrors(async function (req, res, next) {
-    const newUser = await User.create(req.body);
+const signToken = function (payLoad) {
+    const jwtSecretKey = process.env.JWT_SECRET;
+    const jwtExpiresIn = process.env.JWT_EXPIRES_IN;
+    const token = jwt.sign(payLoad, jwtSecretKey, {
+        expiresIn: jwtExpiresIn,
+    });
+    return token;
+};
 
-    return res.status(201).json({
+const signAndSendToken = function (user, statusCode, res) {
+    const token = signToken({ id: user._id });
+    return res.status(statusCode).json({
         status: "success",
+        token: token,
         data: {
-            user: newUser,
+            user: user,
         },
     });
+};
+
+exports.signup = catchAsyncErrors(async function (req, res, next) {
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    signAndSendToken(user, 201, res);
 });
