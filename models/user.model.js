64fd66fb61c1c372cecaf3efc6169c26b5a0258a
Arpit Jana <arpitjana2103@gmail.com>
2025-11-09 12:39:16 +0530

    feature (auth) : password validation

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,16 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 
+const validatePassword = function (password) {
+    return (
+        password.length >= 5 &&
+        password.length <= 20 &&
+        /[A-Z]/.test(password) &&
+        /[a-z]/.test(password) &&
+        /[0-9]/.test(password)
+    );
+};
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -20,8 +30,15 @@ const userSchema = new mongoose.Schema({
     photo: String,
     password: {
         type: String,
-        required: [true, "##-please-provide-a-password-##"],
-        minlength: 8,
+        required: [true, "##-User-Must-Have-A-Password-##"],
+        // Will not work while update..
+        // Only work while creating new Docs..
+        validate: {
+            validator: validatePassword,
+            message:
+                "##-Password-Must-Be-5-To-20-Characters-And-Include-Uppercase,Lowercase-&-Number-##",
+        },
+        select: false,
     },
     passwordConfirm: {
         type: String,
